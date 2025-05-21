db910eba0ff186057abb38ea9939b28a84e9f1a8
lukedunphy <ltdunphy@asu.edu>
2025-05-21 14:26:50 -0400

    adjusted swagger

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+from flask import redirect
 from app import create_app
 from app.models import db
 
 
 app = create_app('ProductionConfig')
+
+@app.route('/', methods=['GET'])
+def index():
+    return redirect('/api/docs')
 
 with app.app_context():
     db.create_all()
